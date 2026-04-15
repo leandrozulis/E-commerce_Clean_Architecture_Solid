6355675e63aa79b1c3c5f2428cbc790064ca03ee
Leandro Zulli Silva <leandro_zulli@hotmail.com>
2026-04-14 22:35:11 -0300

    feat: 05: adiciona configurações do prisma iniciais

---
+++ src/config/prisma.config.ts
@@ -0,0 +1,5 @@
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaClient } from "../generated/prisma/client";
+
+const adapter = new PrismaBetterSqlite3({ url: "file:./db/dev.db" })
+export const prisma = new PrismaClient({ adapter })
